0bc6a0ca9b33e79c60caac77ffc37ae16f852e81
Nsr <es-osama.nsr1318@alexu.edu.eg>
2018-07-03 03:55:24 +0200

    Uploading the project
    uploading the project to submit it

---
+++ Build an Apartment/Assets/_MyScripts/MovementBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace _MyScripts {
+    public class MovementBehaviour : MonoBehaviour {
+        public float Speed = 5f;
+
+        private Vector3 _startPos;
+        private Transform _mainCamTrans;
+
+        private void Start() {
+            _mainCamTrans = Camera.main.transform;
+            _startPos = transform.position;
+        }
+
+        // Update is called once per frame
+        void Update() {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            transform.position += (_mainCamTrans.forward * v + _mainCamTrans.right * h) * Time.deltaTime * Speed;
+        }
+
+        public void ResetPosition() { transform.position = _startPos; }
+    }
+}
---
+++ Build an Apartment/Assets/_MyScripts/ResetPosition.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace _MyScripts {
+	public class ResetPosition : MonoBehaviour {
+		private void OnTriggerExit(Collider other) {
+			if (other.CompareTag("Player")) {
+				other.GetComponent<MovementBehaviour>().ResetPosition();
+			}
+		}
+	}
+}
